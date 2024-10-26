c21dc86f70b25ac8e56d470048827efadbdb6b9b
Pipotka <157719919+Pipotka@users.noreply.github.com>
2024-10-26 12:26:55 +0300

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,58 @@
+import java.lang.Exception
+import java.util.TreeMap
+
+fun main(args: Array<String>) {
+    var X = Input("Введите координату X проверяемой точки: ")
+    var Y = Input("Введите координату Y проверяемой точки: ")
+    var treangle : Triangle = Triangle()
+    treangle.FirstPoint.X = Input("Введите координату X для первой точки треугольника: ")
+    treangle.FirstPoint.Y = Input("Введите координату Y для первой точки треугольника: ")
+    treangle.SecondPoint.X = Input("Введите координату X для второй точки треугольника: ")
+    treangle.SecondPoint.Y = Input("Введите координату Y для второй точки треугольника: ")
+    treangle.ThirdPoint.X = Input("Введите координату X для третьей точки треугольника: ")
+    treangle.ThirdPoint.Y = Input("Введите координату Y для третьей точки треугольника: ")
+    var a = (treangle.FirstPoint.X - X) * (treangle.SecondPoint.Y - treangle.FirstPoint.Y) - (treangle.SecondPoint.X - treangle.FirstPoint.X) * (treangle.FirstPoint.Y - Y)
+    var b = (treangle.SecondPoint.X - X) * (treangle.ThirdPoint.Y - treangle.SecondPoint.Y) - (treangle.ThirdPoint.X - treangle.SecondPoint.X) * (treangle.SecondPoint.Y - Y)
+    var c = (treangle.ThirdPoint.X - X) * (treangle.FirstPoint.Y - treangle.ThirdPoint.Y) - (treangle.FirstPoint.X - treangle.ThirdPoint.X) * (treangle.ThirdPoint.Y - Y)
+
+    if((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+    {
+        println("Точка принадлежит треугольнику")
+    }
+    else
+    {
+        println("Точка не принадлежит треугольнику")
+    }
+
+}
+
+class Point
+{
+    var X : Double = 0.0
+    var Y : Double = 0.0
+}
+
+class Triangle
+{
+    var FirstPoint : Point = Point()
+    var SecondPoint : Point = Point()
+    var ThirdPoint : Point = Point()
+}
+
+fun Input(message : String) : Double{
+    var isCorrect = false
+    var result : Double = 0.0
+    while(!isCorrect)
+    {
+        try {
+            println(message)
+            result = readln().toDouble()
+            isCorrect = true
+        }
+        catch(e : Exception)
+        {
+            println("Ввдено неправильное значение. Значение должно быть в десятичном представлении разделённое точкой")
+        }
+    }
+    return result
+}
